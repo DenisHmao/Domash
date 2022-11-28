9f7dcc8bba0c324c5aa0147709466055ad8b212d
Denis <kamenhm@mail.ru>
2022-11-28 15:40:42 +0500

    zad_10

---
+++ Zad_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write(" Введите 3-х значное число: "); 
+int first = int.Parse(Console.ReadLine());
+int result = (first /10 % 10);
+Console.WriteLine(first + "->" + result); 
